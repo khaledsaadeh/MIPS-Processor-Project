4388f88fbded4533c6a229f35bcf7484eb2ccd03
AhmadArrabi <AhmadZahiArrabi@gmail.som>
2020-11-28 17:10:36 +0200

    Create Top.v

---
+++ Design Template and Sample Test Benches/Top.v
@@ -0,0 +1,14 @@
+/*
+stage1 : instruction fetch (IF)
+stage2 : instruction decode and register fetch (ID)
+stage3 : execuction stage (EXEC)
+stage4 : memory stage (MEM)
+stage5 : writeback stage (WB)
+*/
+
+module Top(PC_VALUE);// testbench holds the PC Value.
+
+
+
+endmodule 
+	
