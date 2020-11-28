cfc1d1cc0a7936455166155de55dcc28ecf3d93d
AhmadArrabi <AhmadZahiArrabi@gmail.som>
2020-11-28 17:09:01 +0200

    Create mux_2to1.v

---
+++ Design Template and Sample Test Benches/mux_2to1.v
@@ -0,0 +1,25 @@
+module mux_2to1(c,a,b,sel);
+	input [31:0]a;
+	input [31:0]b;
+	input sel;
+	output [31:0]c;
+	assign c = (sel==0)?a:b;
+endmodule
+
+module testbench();
+	reg [31:0]x,y;
+	reg sel;
+	wire [31:0]z;
+
+	mux_2to1 myMux(z,x,y,sel);
+	
+	
+initial begin
+	$monitor("z= %b",z);
+	x= 32'h 1;
+	y=0;
+	sel=0;
+	
+end
+
+endmodule
